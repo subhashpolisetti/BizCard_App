068fe938c3c81f62e804d24fb3dcf322f53c1366
Subhash <subhashr161347@gmail.com>
2024-02-05 23:53:11 +0530

    Divider and column added

--- app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,9 +19,13 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -54,24 +60,41 @@ fun CreateBizCard(){
            .height(390.dp)
            .padding(12.dp),
            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-           elevation=CardDefaults.cardElevation(defaultElevation = 4.dp),
-            colors = CardDefaults.cardColors(containerColor= Color.White)
+            colors = CardDefaults.cardColors(containerColor= Color.White),
+           elevation=CardDefaults.cardElevation(defaultElevation = 4.dp)
            ){
-           Surface(
-               modifier = Modifier
-                   .size(150.dp)
-                   .padding(5.dp),
-               shape = CircleShape,
-               border = BorderStroke(0.5.dp, Color.LightGray),
-               tonalElevation = 4.dp,
-//               color = MaterialTheme.colors.primary.copy(alpha = 0.5f)
-           ) {
-                Image(painter = painterResource(id =R.drawable.profile_image ),
-                    contentDescription = "profile image",
-                    modifier = Modifier.size(150.dp),
-                    )
-           }
+          Column (modifier = Modifier
+              .height(300.dp),
+              verticalArrangement = Arrangement.Top,
+              horizontalAlignment = Alignment.CenterHorizontally){
+              CreateImageProfile()
+              Divider()
+              Column (modifier = Modifier.padding(5.dp)){
+                Text(text = "Subhash",
+                    style =MaterialTheme.typography.headlineLarge)
+              }
+
+          }
        }
+    }
+}
+
+@Composable
+private fun CreateImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        tonalElevation = 4.dp,
+//     color = MaterialTheme.colors.primary.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier.size(150.dp),
+        )
     }
 }
 
